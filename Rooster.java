4ea9913dd996318a9e2986d90ab862468a6f2d3f
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 21:07:26 +0800

    Changes for Rooster( I can use inherit This class with Chicken) because a rooster also same like chicken properties.Butas you already mentioned dpnt use inheritance.even though i am extend with parrot but not using for this puprose

--- Rooster.java
+++ Rooster.java
@@ -1,32 +1,20 @@
 package sampletest;
 
-public class Rooster extends Parrot{
-	
-	protected static final String gender ="M";
-protected static Integer value =0;
-	
-	@Override
-	Integer count(Integer i) {
-		System.out.println("Count");
-		super.setValue(i);
-		return value;
-	}
-	
-	
+public class Rooster extends Parrot {
+
+	protected static final String gender = "M";
+
 	@Override
 	public void fly() {
 		System.out.println("I canot fly");
 		count(0);
-	
+
 	}
 
-	
 	@Override
 	public void sing() {
 		System.out.print("cock-adoodle-doo");
 		count(1);
 	}
-	
-	
 
 }
