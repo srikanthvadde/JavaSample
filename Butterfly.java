f467bb85e70054f8d832853d754767a19ca58210
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 21:20:19 +0800

    Updated Butter fly i really didnt understand caterpillar problem.Can u Please explain me indeatil through mail (D.2)

--- Butterfly.java
+++ Butterfly.java
@@ -1,37 +1,16 @@
 package sampletest;
 
-public class Butterfly extends Animal{
-	protected static Integer value =0;
-	
-	 @Override
+public class Butterfly extends Animal {
+	@Override
 	public void sing() {
 		System.out.print("Butterfly doesnot make  Sound");
 		count(0);
 	}
 
-	
-	 @Override
+	@Override
 	public void fly() {
 		System.out.print("Butterfly can fly");
 		count(1);
 	}
-	 
-	 Integer count(Integer i) {
-			System.out.println("Count");
-			super.setValue(i);
-			return value;
-		}
-
-
-	    @Override
-		public Integer getValue() {
-			return value;
-		}
-
-	@Override
-		public void setValue(Integer value) {
-			this.value = value;
-		}
-		
 
 }
