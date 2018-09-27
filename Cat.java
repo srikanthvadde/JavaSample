a63e939de40514a0900a4572d9845494f0c49f84
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 21:18:05 +0800

    Updated for reusable(same like Dog class)

--- Cat.java
+++ Cat.java
@@ -1,23 +1,6 @@
 package sampletest;
 
 public class Cat extends Parrot {
-	protected static Integer value =0;
 	
-	@Override
-	Integer count(Integer i) {
-		System.out.println("Count");
-		super.setValue(i);
-		return value;
-	}
-	
-	@Override
-	public Integer getValue() {
-		return value;
-	}
-
-@Override
-	public void setValue(Integer value) {
-		this.value = value;
-	}
 
 }
