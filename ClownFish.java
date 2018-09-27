a5061b0c32ec27c1f8f5ef742d1e806e77dd2476
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 21:13:45 +0800

    Updated Clownfish class for more reusable

--- ClownFish.java
+++ ClownFish.java
@@ -1,35 +1,12 @@
 package sampletest;
 
-public class ClownFish extends Fish{
-protected static Integer value =0;
-	
-	@Override
-	Integer count(Integer i) {
-		System.out.println("Count");
-		super.setValue(i);
-		return value;
-	}
-	
-	
+public class ClownFish extends Fish {
+
 	@Override
 	void swim() {
 		System.out.println("ClownFish dont swim");
 		count(0);
 	}
-	
-	@Override
-	public void sing() {
-		System.out.print("ClownFish dont sing");
-		count(0);
-	}
-
-	
-	@Override
-	public void walk() {
-		System.out.print("ClownFish dont walk");
-		count(0);
-	}
-	
 
 	public void apperence() {
 		System.out.print("ClownFish are small and Colourful");
@@ -38,6 +15,5 @@ protected static Integer value =0;
 	public void sofHumor() {
 		System.out.print("ClownFish Makes Jokes");
 	}
-
 
 }
