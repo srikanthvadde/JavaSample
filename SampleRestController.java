e9a7d3a0a66cc84e7f30412bbfe90adf18d5d60c
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 22:07:25 +0800

    This is for just sample rest api to get animals.becuase i dont have spring installed .so i just created sample but compilation errors as i dont have spring modules libraries and pom.xml in my code
    Please consider this also

---
+++ SampleRestController.java
@@ -0,0 +1,36 @@
+package sampletest;
+
+
+@RestController
+@RequestMapping("/")
+public class SampleRestController {
+
+	// whenever some one calls our service
+	@GetMapping(value = "birds/{birdid}")
+    public ResponseEntity<Animal> getAllanimals(@PathVariable("birdid") Long id) {
+    	ResponseEntity<Animal> animal = // get the data of animals  from database and process //here need to call url to get the data from another system or data from database and process and need to send back to cleint as well
+		return animal.getBody();
+
+    }
+
+	// whenever we want to call other service
+	@GetMapping(value = "birds/{birdid}")
+	public ResponseEntity<Animal> getAllanimals(@PathVariable("birdid") Long id) {
+		// here need to call url to get the data from another system or data from
+		// database and process and need to send back to cleint as well
+		ClientResource resource = new ClientResource("http://localhost:8080/animals");
+
+		Responseresponse = resource.getResponse();
+
+		if (response.getStatus().isSuccess()) {
+			//process your logic
+			System.out.println("Success! " + response.getStatus());
+			System.out.println(response.getEntity().getText());
+		} else {
+			System.out.println("ERROR! " + response.getStatus());
+			System.out.println(response.getEntity().getText());
+		}
+
+	}
+
+}
