28e3c83de82aa202d82e42ac05b62367c6896409
srikanthvadde <31717083+srikanthvadde@users.noreply.github.com>
2018-09-27 21:05:05 +0800

    Changes done remove unwanted code
    considered says as sing

--- Chicken.java
+++ Chicken.java
@@ -1,34 +1,19 @@
 package sampletest;
 
-public class Chicken extends Animal{
-	
-	protected static final String gender="F";
-protected static Integer value =0;
-	
-	@Override
-	Integer count(Integer i) {
-		System.out.println("Count");
-		super.setValue(i);
-		return value;
-	}
-	
-	
+public class Chicken extends Bird {
 
-
+	protected static final String gender = "F";
 
 	@Override
-	void sing() {
+	public void sing() {
 		System.out.println("Cluck,cluck");
 		count(1);
 	}
-	
+
 	@Override
 	public void fly() {
-		System.out.print("i Cannot fly");
+		System.out.print("Chicken  Cannot fly");
 		count(0);
 	}
 
-	
-
-	
 }
